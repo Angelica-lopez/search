6f8c9ca025b5b15db5240cfde985071056004b5b
Angelica <angielopez4004@gmail.com>
2021-04-25 17:26:45 -0500

    search

---
+++ index.js
@@ -0,0 +1,18 @@
+const input = document.querySelector(".content__seeker-space");
+const button = document.querySelector(".content__seeker-button");
+const guest = ["jorge", "lizet", "nicole"];
+
+
+button.addEventListener('click', function() {
+    for(i=0; i<=2; i++) {
+        if(guest.includes( input.value) ) {
+            alert("Felicidades si has sido invitado");
+            break;
+        }else {
+            alert("Lo siento no has sido invitado :(");
+            break;
+        }
+    }
+})
+
+
